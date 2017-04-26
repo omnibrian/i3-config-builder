e34f371fc0e8d4d838cbb92216e8df9770284e2d
Brian LeBlanc <bleblan2@unb.ca>
2017-04-26 01:21:50 -0300

    Update setup files

--- setup.py
+++ setup.py
@@ -9,21 +9,22 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='i3-config-builder',
-    version='0.1',
-    description='Command Line i3 config builder',
-    long_description=long_description,
-    author='Brian LeBlanc',
-    author_email='brileb73@gmail.com',
-    license='MIT',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    install_requires=['argparse'],
-    extras_require={
+    name = 'i3-config-builder',
+    version = '0.1',
+    description = 'Command Line i3 config builder',
+    long_description = long_description,
+    author = 'Brian LeBlanc',
+    author_email = 'brileb73@gmail.com',
+    url = 'https://github.com/brileb73/i3-config-builder',
+    license = 'MIT',
+    packages = find_packages(exclude = ['contrib', 'docs', 'tests']),
+    install_requires = ['argparse'],
+    extras_require = {
         ':python_version=="2.6"': [
             'argparse>=1.1'
         ]
     },
-    entry_points={
+    entry_points = {
         'console_scripts': [
             'i3cfgbuild=i3cfgbuild.main:main'
         ]
